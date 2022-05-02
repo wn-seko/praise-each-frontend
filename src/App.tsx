217c7052ecc6f0fc881afcf63a123061761b72c0
wn-seko <wn.seko@gmail.com>
2022-05-03 02:47:27 +0900

    remove semantic-ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import Routes from '~/Routes';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import 'normalize.css/normalize.css';
-import 'semantic-ui-css/semantic.min.css';
-import 'react-dropdown/style.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'emoji-mart/css/emoji-mart.css';
 

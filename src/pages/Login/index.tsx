08c5bbd19fd31ab3aba61534a916ecfab31f3380
wn-seko <wn.seko@gmail.com>
2022-05-03 01:20:59 +0900

    change LoginPage style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,37 +1,41 @@
 import React, { FC } from 'react';
-import { Center, Flex, Spacer, Heading, Skeleton } from '@chakra-ui/react';
+import { Box, Center, Flex, Spacer, Heading, Skeleton } from '@chakra-ui/react';
 import { GithubButton, GoogleButton } from '~/components/ui/Button';
 import { useLogin } from './hooks';
+import { getThemeColor } from '~/layouts/theme';
 
 const LoginPage: FC = () => {
   const { loading, urls } = useLogin();
 
   return (
     <Center w="100vw" h="100vh">
-      <Flex direction="column" align="center" justify="center" gap="1rem">
-        <Heading>Praise Each</Heading>
-        <Spacer h="1rem" />
-        <Flex direction="column" gap="1rem">
-          {loading ? (
-            <>
-              <Skeleton w="200px" h="40px" />
-            </>
-          ) : (
-            <>
-              {urls.github && (
-                <GithubButton w="100%" as="a" href={urls.github}>
-                  GitHub アカウントでログイン
-                </GithubButton>
-              )}
-              {urls.google && (
-                <GoogleButton w="100%" as="a" href={urls.google}>
-                  Google アカウントでログイン
-                </GoogleButton>
-              )}
-            </>
-          )}
+      <Box padding={8} borderRadius={8} border="solid 4px" borderColor={getThemeColor('primary')}>
+        <Flex direction="column" align="center" justify="center" gap="1rem">
+          <Heading>Praise Each</Heading>
+          <Box borderColor={getThemeColor('border')}>みんなで感謝を共有しよう！</Box>
+          <Spacer h="1rem" />
+          <Flex direction="column" gap="1rem">
+            {loading ? (
+              <>
+                <Skeleton w="200px" h="40px" />
+              </>
+            ) : (
+              <>
+                {urls.github && (
+                  <GithubButton w="100%" as="a" href={urls.github}>
+                    GitHub アカウントでログイン
+                  </GithubButton>
+                )}
+                {urls.google && (
+                  <GoogleButton w="100%" as="a" href={urls.google}>
+                    Google アカウントでログイン
+                  </GoogleButton>
+                )}
+              </>
+            )}
+          </Flex>
         </Flex>
-      </Flex>
+      </Box>
     </Center>
   );
 };
